204f2521d28e2ed21a6ab5cbf251d81fc5db4b1a
anmolbaidwan <anmolbaidwan@gmail.com>
2024-10-26 14:55:34 -0700

    Created branch for Proof of Concept

--- surrounded/Assets/Scripts/UIX/MainMenu.cs
+++ surrounded/Assets/Scripts/UIX/MainMenu.cs
@@ -7,8 +7,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayButton(){
-        SceneManager.LoadScene("Game");
+    public void BossButton(){
+        SceneManager.LoadScene("Bosses");
+    }
+    public void ItemButton(){
+        SceneManager.LoadScene("Items");
+    }
+    public void EnemyButton(){
+        SceneManager.LoadScene("Enemies");
+    }
+    public void BoundaryButton(){
+        SceneManager.LoadScene("World Boundary");
     }
     public void QuitButton(){
         Application.Quit();
