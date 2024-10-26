925f6f0ed67f840ca512af6d7fa8be3533509f57
anmolbaidwan <anmolbaidwan@gmail.com>
2024-10-26 14:45:01 -0700

    Fixed Stats Stacking bug
    Diverge reduces fire rate, shield updates correctly each round, same with machineGun and RocketBoost

--- surrounded/Assets/Scripts/Player/PlayerController.cs
+++ surrounded/Assets/Scripts/Player/PlayerController.cs
@@ -9,11 +9,11 @@ public class PlayerController : MonoBehaviour
     public Weapon weapon;
     public GameOverScreen gameOverScreen;     // Reference to the GameOverScreen script
     public PauseMenu pauseMenu;
-    public float damage = 5f, defense = 3, moveSpeed = 10f, health = 50, maxHealth = 50, XP = 0, shield = 0, maxShield;
+    public float damage = 5f, defense = 3, moveSpeed = 10f, baseSpeed = 10f, health = 50, maxHealth = 50, XP = 0, shield = 0, maxShield;
     public int currentLevel = 1;
     public float levelReq = 30 * Mathf.Pow(1.1f, 0);
     public SpriteRenderer sprite;
-    private int MachineGunCount = 0, RocketBoosterCount = 0;
+    private int MachineGunCount = 0, RocketBoosterCount = 0, divergeCount = 0, shieldCount = 0;
     public TMP_Text sstats, stats; //stats for upgrade page and stats page
     public bool divergeActivated = false;
 
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetMouseButton(0)){
             if(divergeActivated){
-                weapon.Diverge();
+                weapon.Diverge(divergeCount);
             } else{
                 weapon.Fire();
             }
@@ -77,9 +77,13 @@ public class PlayerController : MonoBehaviour
     private void LevelUp() {
         damage += 1;
         defense += 1;
+        baseSpeed += 1;
         moveSpeed += 1;
         health += 10;
         maxHealth += 10;
+        if(shieldCount != 0){
+            maxShield = (0.1f+(0.05f*shieldCount))*maxHealth;
+        }
         XP -= levelReq;
         currentLevel++;
         levelReq = 30 * Mathf.Pow(1.1f, (currentLevel-1));
@@ -96,7 +100,7 @@ public class PlayerController : MonoBehaviour
         if(MachineGunCount == 0){
             weapon.fireRate *= 1.1f;
         } else{
-            weapon.fireRate *= 1.05f;
+            weapon.fireRate = (1.1f+(0.05f*MachineGunCount)) * weapon.baseRate;
         }
         MachineGunCount++;
         }
@@ -105,7 +109,7 @@ public class PlayerController : MonoBehaviour
         if(RocketBoosterCount == 0){
             moveSpeed *= 1.1f;
         } else{
-            moveSpeed *= 1.05f;
+            moveSpeed = (1.1f+(0.05f*RocketBoosterCount)) * baseSpeed;
         }
         RocketBoosterCount++;
     }
@@ -114,9 +118,10 @@ public class PlayerController : MonoBehaviour
         XP += levelReq;
         XP += levelReq;
     }
-
+    
     public void DivergeActivated(){
         divergeActivated = true;
+        divergeCount++;
     }
 
     public void Stats(){
@@ -125,6 +130,9 @@ public class PlayerController : MonoBehaviour
     }
 
     public void Shield(){   
-        maxShield = shield = 0.1f*health;
+        maxShield = (0.1f+(0.05f*shieldCount))*maxHealth;
+        shieldCount++;
+        shield = maxShield;
+        moveSpeed = (1-(0.05f*shieldCount))*moveSpeed;
     }
 }
