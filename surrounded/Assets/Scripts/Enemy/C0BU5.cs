0f26279016902821667aa795a0587d71c8b6cdb9
anmolbaidwan <anmolbaidwan@gmail.com>
2024-10-31 12:31:08 -0700

    World Boundary Demo DONE
    Quetzal js Needs to Add Comet

--- surrounded/Assets/Scripts/Enemy/C0BU5.cs
+++ surrounded/Assets/Scripts/Enemy/C0BU5.cs
@@ -14,7 +14,7 @@ public class C0BU5Boss : MonoBehaviour
     }
     IEnumerator C0BU5Spawn(){
         yield return new WaitForSeconds(5);
-        Vector2 spawnPosition = new Vector2(0f, 1.5f); // Get off-screen spawn position
+        Vector2 spawnPosition = new Vector2(0f, 0f); // Get off-screen spawn position
         GameObject COBUS = Instantiate(C0BU5, spawnPosition, Quaternion.identity);
         yield break;
     }
--- surrounded/Assets/Scripts/GameControl/WorldBoundry.cs
+++ surrounded/Assets/Scripts/GameControl/WorldBoundry.cs
@@ -2,35 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class WorldBoundary : MonoBehaviour
 {
    public GameOverScreen gameOverScreen;
-   private Coroutine dot;
+   private PlayerController p;
+   public GameObject player, comet;
+   private Camera mainCamera;            // Reference to the main camera
+   private bool isOutOfBounds = true;
+   public TMP_Text warningText;
 
-   public void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            PlayerController target = other.gameObject.GetComponent<PlayerController>();
-            float dmg = (target.health)/3;
-            dot = StartCoroutine(DOT(target,dmg));
-         }
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            EnemyController enemy = other.gameObject.GetComponent<EnemyController>();
-            enemy.takeDamage(enemy.health/2);
+    void Start(){
+        mainCamera = Camera.main;         // Get the main camera
+    }
+    void Update(){
+        if(player!= null){
+            float distance = Vector3.Distance (player.transform.position, transform.position);
+            if(distance >= 550 && isOutOfBounds){
+                StartCoroutine(Warning());
+                isOutOfBounds = false;
+            } else if( distance < 550){
+                StopCoroutine(Warning());
+                warningText.text = "";
+                isOutOfBounds = true;
+            }
         }
     }
-    public void OnCollisionExit2D(Collision2D other){
-        StopCoroutine(dot);
+    Vector2 GetOffScreenPosition()
+    {
+        Vector2 screenMin = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 screenMax = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
+        float x, y;
+        bool spawnOnX = Random.value > 0.5f;
+
+        if (spawnOnX)
+        {
+            x = Random.value < 0.5f ? screenMin.x - 2 : screenMax.x + 2;
+            y = Random.Range(screenMin.y, screenMax.y);
+        }
+        else
+        {
+            x = Random.Range(screenMin.x, screenMax.x);
+            y = Random.value < 0.5f ? screenMin.y - 2 : screenMax.y + 2;
+        }
+
+        return new Vector2(x, y);
     }
-    private IEnumerator DOT(PlayerController target, float damage){
-        while(target != null){
-            target.takeDamage(damage);
+
+    IEnumerator Warning(){
+        for(int i = 5; i>=0; i--){
+            warningText.text = "WARNING: PLAYER REACHING OUT OF BOUNDS.\n\n" + i.ToString() + " SECONDS BEFORE COMET DEPLOYMENT";
             yield return new WaitForSeconds(1);
         }
+        if(!isOutOfBounds){
+            Debug.Log("Comet INCOMING");
+            comet = Instantiate(comet, GetOffScreenPosition(), Quaternion.identity);
+            warningText.text = "";
+        }
+        yield break;
     }
-
-    
 }
