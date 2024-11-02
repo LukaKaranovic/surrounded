8978525a9da2eb2773c5c25eb87c34a5f5fff374
Q-ACT <79498711+Q-ACT@users.noreply.github.com>
2024-11-02 11:40:02 -0700

    Removed C0BU5Hand inheritance from EnemyController

--- surrounded/Assets/Scripts/Enemy/C0BU5/C0BU5Hand.cs
+++ surrounded/Assets/Scripts/Enemy/C0BU5/C0BU5Hand.cs
@@ -3,30 +3,63 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class CobusHand : EnemyController
+public class CobusHand : MonoBehaviour
 {
+    public Transform firePoint;
+    public float ContactDamage;
+    public float maxSpeed;
+
+    private Rigidbody2D rb;
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player"); // Assuming the player has the tag "Player"
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {   
+        if (player != null)
+        {
+            MoveTowardsPlayer();
+            RotateToPlayer();
+        }
+    }
     protected void RotateToPlayer()
     {
         Vector2 aimDirection = player.transform.position - transform.position;
         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg + 90;
         rb.rotation = aimAngle;
     }
-        void FixedUpdate()
-    {   
-        if (player != null)
+    protected void MoveTowardsPlayer() //moves enemy towards player in a straight line
+    {
+        Vector3 heading = ((player.transform.position) - transform.position).normalized;
+        if (rb.velocity.magnitude > maxSpeed)
         {
-            float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);  
-            MoveTowardsPlayer();
-            RotateToPlayer();
-            if (distanceToPlayer <= shootingRange && Time.time >= nextFireTime)
-            {
-                ShootAtPlayer(damage);
-                nextFireTime = Time.time + 1f / fireRate;
-            }
+            rb.AddForce(maxSpeed * -rb.velocity.normalized, ForceMode2D.Impulse);
         }
+        rb.AddForce(maxSpeed * heading, ForceMode2D.Impulse);
     }
 
-    public new void OnCollisionEnter2D(Collision2D other)
+    private void moveToPosition(Vector3 position)
     {
+        Vector3 heading = ((position) - transform.position).normalized;
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.AddForce(maxSpeed * -rb.velocity.normalized, ForceMode2D.Impulse);
+        }
+        rb.AddForce(maxSpeed * heading, ForceMode2D.Impulse);
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // get enemy
+            PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+            // damage both heavily
+            player.takeDamage(ContactDamage);
+        }
     }
 }
