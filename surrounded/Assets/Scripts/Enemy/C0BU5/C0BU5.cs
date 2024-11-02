e231c841f9d64e3bbfcdcc457540058a7a2ffc39
Q-ACT <79498711+Q-ACT@users.noreply.github.com>
2024-11-02 14:41:33 -0700

    Smoothed C0B-U5 Strafing

--- surrounded/Assets/Scripts/Enemy/C0BU5/C0BU5.cs
+++ surrounded/Assets/Scripts/Enemy/C0BU5/C0BU5.cs
@@ -8,17 +8,19 @@ public class C0BU5 : MonoBehaviour
     private GameObject player;
     private Rigidbody2D rb;
     private Vector3 hoverOffset;
-    private float moveSpeed;
+    private float maxSpeed;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        moveSpeed = player.GetComponent<PlayerController>().moveSpeed/2;
+        maxSpeed = player.GetComponent<PlayerController>().moveSpeed/2;
     }
 
     void HoverPlayer()
     {
         Vector3 target = new(player.transform.position.x, transform.position.y, 0f);
-        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        float speed = maxSpeed;
+        speed -= maxSpeed / (Mathf.Abs(transform.position.x - target.x) + 1);
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
 
     void FixedUpdate()
