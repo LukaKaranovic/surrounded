a235cccf2a7126e8d01540fb16cdd92b236448e1
Q-ACT <79498711+Q-ACT@users.noreply.github.com>
2024-11-02 12:39:47 -0700

    C0B-U5 Hovers above player

--- surrounded/Assets/Scripts/Enemy/C0BU5/C0BU5.cs
+++ surrounded/Assets/Scripts/Enemy/C0BU5/C0BU5.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Cinemachine.CinemachineOrbitalTransposer;
 
 public class C0BU5 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject player;
+    private Rigidbody2D rb;
+    private Vector3 hoverOffset;
+    private float moveSpeed;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        moveSpeed = player.GetComponent<PlayerController>().moveSpeed/2;
     }
 
-    // Update is called once per frame
-    void Update()
+    void HoverPlayer()
     {
-        
+        Vector3 target = new(player.transform.position.x, transform.position.y, 0f);
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        HoverPlayer();
     }
 }
