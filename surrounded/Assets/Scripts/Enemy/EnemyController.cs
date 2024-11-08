041b50e4abeca531aa4b1c026e944fd9bf0485bc
Q-ACT <79498711+Q-ACT@users.noreply.github.com>
2024-11-08 12:53:46 -0800

    Removed Enemy Avoidance Radius Scaling
    Removed the Avoidance radius behavior which scales the radius relative to the enemies current velocity. Also moved camera shake script into the UIX folder

--- surrounded/Assets/Scripts/Enemy/EnemyController.cs
+++ surrounded/Assets/Scripts/Enemy/EnemyController.cs
@@ -94,15 +94,14 @@ public class EnemyController : MonoBehaviour
         rb.AddForce(maxSpeed * heading,ForceMode2D.Impulse);
     }
 
-    protected Vector3 avoidanceAdjustment(Vector3 heading){
-        float avoidanceRadiusAdjusted = avoidanceRadius + rb.velocity.magnitude/2; 
-        Collider2D[] NearbyColliders = Physics2D.OverlapCircleAll(transform.position, avoidanceRadiusAdjusted);
+    protected Vector3 avoidanceAdjustment(Vector3 heading){ 
+        Collider2D[] NearbyColliders = Physics2D.OverlapCircleAll(transform.position, avoidanceRadius);
         foreach (Collider2D collider in NearbyColliders)
         {    
             if (collider.gameObject != this.gameObject && !collider.gameObject.CompareTag("Bullet"))
             {
                 float distance = Vector2.Distance(transform.position, collider.transform.position);
-                heading += avoidanceRadiusAdjusted / distance * Vector3.Normalize(transform.position - collider.transform.position);
+                heading += avoidanceRadius / distance * Vector3.Normalize(transform.position - collider.transform.position);
             }
         }
         heading = heading.normalized;
