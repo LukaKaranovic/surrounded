91257467f6820d982ebffc47bad4b97b8e4b677a
Connor McDermid <mcdermidconnor@outlook.com>
2024-11-20 14:44:47 -0800

    Test proof of concept -- test passes
    Signed-off-by: Connor McDermid <mcdermidconnor@outlook.com>

--- surrounded/Assets/Tests/DamageTest.cs
+++ surrounded/Assets/Tests/DamageTest.cs
@@ -3,11 +3,19 @@ using Player;
 using GameControl;
 using NUnit.Framework;
 using TMPro;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 namespace Tests
 {
     public class DamageTest
     {
+
+        [SetUp]
+        public void Setup()
+        {
+            SceneManager.LoadScene("Scenes/Game");
+        }
         // A UnityTest behaves like a coroutine in PlayMode
         // and allows you to yield null to skip a frame in EditMode
         [UnityEngine.TestTools.UnityTest]
@@ -19,19 +27,16 @@ namespace Tests
             newcam.tag = "MainCamera";
             PlayerStats s = ScriptableObject.CreateInstance<PlayerStats>();
             s.ResetStats(); // give player a Stats object*/
-            
-            GameObject player = GameObject.Instantiate(Resources.Load("Prefab/Player Ship"),
-                Vector2.zero,
-                Quaternion.identity)
-                as GameObject;
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
             Assert.That(player, !Is.Null);
             GameObject bullet = GameObject.Instantiate(Resources.Load("Prefab/PiercingBullet"),
                 new Vector3(player.transform.position.x, player.transform.position.y + 3), Quaternion.identity) as GameObject;
             Assert.That(bullet, !Is.Null);
-            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.down, ForceMode2D.Impulse);
+            bullet.GetComponent<Bullet>().damage = 1;
+            bullet.GetComponent<Rigidbody2D>().AddForce(Vector2.down * 4, ForceMode2D.Impulse);
             yield return new WaitForSeconds(4);
             Assert.That(player.GetComponent<PlayerController>().health 
-                        < player.GetComponent<PlayerController>().stats.maxHealth);
+                        < player.GetComponent<PlayerController>().stats.maxHealth, Is.True);
             yield return true;
         }
     }
