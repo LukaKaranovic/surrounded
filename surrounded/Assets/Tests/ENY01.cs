4e0c3ed5af4ccfbdf531ed4f43ccd5af0909bdb7
Code-Persona <CFAdams10@gmail.com>
2024-11-20 21:16:19 -0800

    Added EVY01
    Blah added enemy spawn check

---
+++ surrounded/Assets/Tests/ENY01.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using GameControl;
+using Player;
+using Enemy;
+
+
+namespace Tests
+{
+    public class ENY01 
+    {
+        private Keyboard keyboard;
+        private Mouse mouse;
+        public bool enemiesAlive = false;
+
+        [SetUp]
+        public new void Setup()
+        {
+            SceneManager.LoadScene("Scenes/Game"); // load game scene
+            keyboard = InputSystem.AddDevice<Keyboard>();
+            mouse = InputSystem.AddDevice<Mouse>();
+
+        }
+        [UnityTest]
+        public IEnumerator ENY01WithEnumeratorPasses() {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            PlayerController pc = player.GetComponent<PlayerController>();
+            Assert.That(player, !Is.Null);
+            GameObject RT = GameObject.Find("Canvas/Timer");
+            RoundTimer timer = RT.GetComponent<RoundTimer>();
+            Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            Assert.That(player, !Is.Null);
+            if (Enemies != null) 
+            {
+                enemiesAlive = true;
+                Assert.IsNotNull(Enemy);
+            }
+            yield return new WaitForSeconds(5);
+            Assert.That(Enemies = null);
+
+        }
+
+    }
+}
